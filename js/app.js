925b1e3538f1f01f0d2842705c87ef83107248d2
pracniscate <lorena.giddens@outlook.com>
2018-11-26 22:26:34 -0500

    fetch the name, username, repo count, and URL of any github user

--- js/app.js
+++ js/app.js
@@ -7,3 +7,29 @@ const urlContainer = document.querySelector(".main__profile-url");
 
 const client_id = "Iv1.4ef43884abcd35a9";
 const client_secret = "34979db0810d3be33c9b4e1a03505d82540b9e71";
+
+// make an API call
+// use async await
+const fetchUsers = async (user) => {
+    // use fetch API
+    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret${client_secret}`);
+    // convert the call into JSON
+    const data = await api_call.json();
+    // return an object with the key called data & setting its value to const data
+    // if key & value pairs are the same, simply call data once
+    return { data }
+};
+
+// calls fetchUsers function
+const showData = () => {
+    fetchUsers(inputValue.value).then((response) => {
+        nameContainer.innerHTML = `Name: <span class="main__profile-value">${response.data.name}</span>`
+        unContainer.innerHTML = `Username: <span class="main__profile-value">${response.data.login}</span>`
+        reposContainer.innerHTML = `Repos: <span class="main__profile-value">${response.data.public_repos}</span>`
+        urlContainer.innerHTML = `URL: <span class="main__profile-value">${response.data.html_url}</span>`
+    })
+};
+
+searchButton.addEventListener("click", () => {
+    showData();
+})
