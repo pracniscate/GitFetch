8ff6bdb4270d7b97774a8f63e7ccef09bd322e80
pracniscate <lorena.giddens@outlook.com>
2018-11-26 21:24:51 -0500

    register github api

--- js/app.js
+++ js/app.js
@@ -4,3 +4,6 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = "Iv1.4ef43884abcd35a9";
+const client_secret = "34979db0810d3be33c9b4e1a03505d82540b9e71";
